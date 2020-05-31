69411258f1c33c2d6bb627817711fc26b43d6ced
marcrleonard <marc.r.leonard@gmail.com>
2020-05-31 15:38:34 -0600

    working on code example

--- example.py
+++ example.py
@@ -12,8 +12,17 @@ for pp in range(200):
 	x = random.randint(0, Canvas.width)
 	y = random.randint(0, 200)
 
-	p = Point(x,y)
+	coords = (x,y)
+	points.append(coords)
+
+	p = Point(*coords)
 	p.stroke_color='white'
 
+points.sort(key = lambda x: x[0])
+for idx in range(0, len(points), 10):
+	p_start_x, p_start_y = points[idx]
+
+	l = Path([(p_start_x, 220),(p_start_x, 500)])
+	l.stroke_color = 'white'
 
 Canvas.save(f'example.svg')
