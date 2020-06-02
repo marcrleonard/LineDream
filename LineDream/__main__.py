4d1a8431cd0d9b73b7323a111b4f127862ef43f1
marcrleonard <marc.r.leonard@gmail.com>
2020-06-02 16:57:51 -0600

    add files

---
+++ LineDream/__main__.py
@@ -0,0 +1,7 @@
+from .shapes.Path import Path
+from .shapes.Ellipse import Circle, Ellipse, Point
+from .shapes.Rectangle import Rectangle, Square
+from .enviornment.Canvas import Canvas
+
+def main():
+	print('entry point')
