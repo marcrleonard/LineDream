5796fc500765321307c0ca67644373a40d4ace4e
marcrleonard <marc.r.leonard@gmail.com>
2020-06-07 09:55:05 -0600

    remove lxml

--- LineDream/shapes/hershey/hershey.py
+++ LineDream/shapes/hershey/hershey.py
@@ -19,7 +19,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-from lxml import etree
+# from lxml import etree
 
 from .aux_hershey import hersheydata
 from .aux_hershey import inkex
