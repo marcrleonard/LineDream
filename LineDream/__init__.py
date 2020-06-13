8384c4087e9f25092b8a9031fd7330e2716de200
marcrleonard <marc.r.leonard@gmail.com>
2020-06-12 19:15:06 -0600

    rename class

--- LineDream/__init__.py
+++ LineDream/__init__.py
@@ -3,5 +3,5 @@ from .primitives.Ellipse import Circle, Ellipse, Point
 from .primitives.Rectangle import Rectangle, Square
 from .enviornment.Canvas import Canvas
 from .helpers.CircleMath import CircleMath
-from .primitives.TextPaths import TextPaths
+from .primitives.TextLine import TextLine
 # from .enviornment.Tweaks import Tweaks
