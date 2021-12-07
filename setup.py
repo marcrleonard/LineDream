314978b3144f29cb788d18cd9b1116683ebdce1b
marcrleonard <marc.r.leonard@gmail.com>
2021-12-07 11:32:26 -0700

    add lxml

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ REQUIRES_PYTHON = '>=3.7.0'
 # What packages are required for this module to be executed?
 REQUIRED = [
     'drawSvg',
-    'numpy'
+    'numpy',
+    'lxml'
 ]
 
 # What packages are optional?
