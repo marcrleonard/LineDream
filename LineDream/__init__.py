178255ca79649309ba75244a040daf87fb1ddcb7
marcrleonard <marc.r.leonard@gmail.com>
2021-12-10 16:14:49 -0700

    update init with docs

--- LineDream/__init__.py
+++ LineDream/__init__.py
@@ -1,7 +1,48 @@
 from .primitives.Line import Line
-from .primitives.Ellipse import Circle, Ellipse, Point
+from .primitives.BaseShape import BaseShape
+from .primitives.Ellipse import Circle, Ellipse, Point, Arc
 from .primitives.Rectangle import Rectangle, Square
-from .enviornment.Canvas import Canvas
+from .enviornment.Canvas import _Canvas
 from .helpers.CircleMath import CircleMath
-from .primitives.TextLine import TextLine
+# from .primitives.TextLine import TextLine
 # from .enviornment.Tweaks import Tweaks
+
+# def parse_readme():
+# 	rv = open('../README.md').readlines()
+#
+#
+#
+# 	# print(rv)
+# 	if '.svg' in rv[0]:
+# 		rv.pop(0)
+# 		if '\n' in rv[0]:
+# 			rv.pop(0)
+#
+# 	return ''.join(rv)
+#
+# __doc__ = parse_readme()
+
+Canvas = _Canvas
+"""
+Canvas Singleton Object. This controls the output 'canvas', and rendering of the draw loop.
+
+# Attributes
+	width
+	height
+"""
+# Note, I had to do this Canvas=_Canvas thing so these ^ doc strings would work.
+
+__all__ = [
+	'Line',
+	'Circle',
+	'Ellipse',
+	'Arc',
+	'Point',
+	'Rectangle',
+	'Square',
+	'CircleMath',
+	'BaseShape',
+	'Canvas',
+]
+
+
